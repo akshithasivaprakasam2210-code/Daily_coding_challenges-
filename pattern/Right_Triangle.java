ef9b52006af164d927d5aebf4c878c9648260014
Akshitha Sivaprakasam <akshithasivaprakasam2210@gmail.com>
2026-07-22 21:52:25 +0530

    Solved problems in java

---
+++ pattern/Right_Triangle.java
@@ -0,0 +1,10 @@
+class Right_Triangle {
+    public static void main (String[] args ){
+        for (int i = 1;i<=5;i++){
+            for (int j = 1 ; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
