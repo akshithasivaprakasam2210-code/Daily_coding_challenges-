992dc2635d4b6cb2f84238c01b5e64a778a9cc1d
Akshitha Sivaprakasam <akshithasivaprakasam2210@gmail.com>
2026-07-24 09:52:53 +0530

    solved problems for day10

---
+++ day10/InrersectionOfTwoNumbers.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int count = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            for (int j = 0; j < nums2.length; j++) {
+
+                if (nums1[i] == nums2[j]) {
+
+                    boolean alreadyPresent = false;
+
+                    for (int k = 0; k < count; k++) {
+                        if (temp[k] == nums1[i]) {
+                            alreadyPresent = true;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyPresent) {
+                        temp[count] = nums1[i];
+                        count++;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return Arrays.copyOf(temp, count);
+    }
+}
